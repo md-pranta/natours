3d1d76a6ffd7720e4f2b438e82dca128a136fef3
md-pranta <juhurenurulhaque@gmail.com>
2023-12-02 16:13:32 +0600

    project natours setup and introduce express

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const morgan = require('morgan');
+
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
+
+const app = express();
+
+//first middleware
+app.use(morgan('dev'));
+app.use(express.json());
+app.use((req, res, next) => {
+  // console.log('from middleware');
+  req.requestTime = new Date().toISOString();
+  next();
+});
+
+//all routes handeler
+
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
+//our server start
+module.exports = app;
